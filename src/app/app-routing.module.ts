a26b283c81b5eac9485b02c2174bf39f57a145a2
Rosangela <rosangela-bsilva@hotmail.com>
2022-03-11 00:02:28 -0300

    rotas e componente menu/nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path:'home',
+    loadChildren:()=> import('./home/home.module').then((mod) => mod.HomeModule),
+  },
+  {
+    path:'technologies',
+    loadChildren: ()=> import('./technologies/technologies.module').then((m)=> m.TechnologiesModule),
+  },
+  // {
+  //   path:'experiences',
+  //   loadChildren: ()=> import('./animais/animais.module').then((m)=> m.AnimaisModule),
+  // },
+  // {
+  //   path:'contact',
+  //   loadChildren: ()=> import('./animais/animais.module').then((m)=> m.AnimaisModule),
+  // }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
